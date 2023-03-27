b1fea3ddcd63c58391132c6de408cfdba39d7039
Necnion8 <necnionch@gmail.com>
2023-03-27 15:59:45 +0900

    Impl: consoleパネルの実装とxterm.jsを使った試作

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+import asyncio
 import mimetypes
 from http import HTTPStatus
 from pathlib import Path
 
 from flask import Flask, render_template, request, redirect, Response, send_file
+from flask_socketio import SocketIO, emit
 from werkzeug.utils import secure_filename
 
+from serverprocess import ServerProcess
+
 app = Flask(__name__)
+sio = SocketIO(app, cors_allowed_origins="*")  # 実際の設定に
 
 # ファイルエクスプローラのRootディレクトリ (これより上の階層には移動できない(はず))
 FILE_EXPLORER_ROOT = Path("D:\\Minecraft Launcher 4").resolve()
+
+test_server = ServerProcess("java", "-Xmx128M", "-Xms128M", "-jar", "paper-1.8.8-445.jar",
+                            cwd="P:\\tmpTinySpigot")
 
 
 def is_safe_path(path: Path):
@@ -119,5 +127,37 @@ def file_io():
         return send_file(FILE_EXPLORER_ROOT / path, mimetype=mimetypes.guess_extension(path.name))
 
 
+@app.route("/console")
+def console():
+    return render_template("console.html")
+
+
+@sio.on("connect")
+def _ws_connect(_):
+    # hard split
+    data = test_server.stdin_content[-1000 * 1000:].decode("utf-8", errors="ignore")
+    emit("term_data", dict(raw=data))
+
+
+@sio.on("disconnect")
+def _ws_disconnect():
+    pass
+
+
+@sio.on("term_send_text")
+def _ws_term_send_text(data):
+    text = data["text"].strip()
+    if not text:
+        return
+    emit("term_data", dict(raw=f"[WebConsole]: /{text}\n\r"), broadcast=True)
+    test_server.write(text + "\n")
+
+
 if __name__ == "__main__":
-    app.run()
+    def _send(data: bytes):
+        sio.emit("term_data", dict(raw=data.decode("utf-8")))
+
+    test_server.add_stdin_listener(_send)
+    test_server.start()
+
+    sio.run(app, debug=False, use_evalex=False, allow_unsafe_werkzeug=True)
